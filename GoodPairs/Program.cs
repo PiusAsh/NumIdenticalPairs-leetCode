eff5b0dc5c8839a3c4ce545d0fa12703fc1b3de0
PiusAsh <Pius1ash@gmail.com>
2022-08-24 09:09:49 +0100

    Add project files.

---
+++ GoodPairs/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace GoodPairs
+{
+
+    class Program
+    {
+        public static int NumIdenticalPairs(int[] nums)
+        {
+            int count = 0;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                for (int j = i + 1; j < nums.Length; j++)
+                {
+                    if (nums[i] == nums[j] && i < j)
+                    {
+                        count++;
+                    }
+                }
+            }
+            return count;
+        }
+
+        static void Main(string[] args)
+        {
+            int[] nums = { 1, 2, 3, 1, 1, 3 };
+            int result = NumIdenticalPairs(nums);
+            Console.WriteLine(result);
+        }
+    }
+}
